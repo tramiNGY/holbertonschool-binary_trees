4bf22bf565bfcde8e2bce4fe1d3e54fe819f857e
totomus83 <tom.dibellonio@gmail.com>
2024-12-10 17:02:05 +0100

    first draft of ex9

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - measures the height of a tree
+ * @tree: pointer to the root
+ * Return: either i or j depending on the biggest one, -1 if fail
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t i;
+	size_t j;
+
+	if (tree == NULL)
+		return (-1);
+
+	i = (binary_tree_height(tree->left)) + 1;
+	j = (binary_tree_height(tree->right)) + 1;
+
+	if (i > j)
+	{
+		return (i);
+	}
+	else
+	{
+		return (j);
+	}
+}
