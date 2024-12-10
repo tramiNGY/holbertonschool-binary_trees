fa85cd649fec80ba477ed94a905eb8505acaf381
totomus83 <tom.dibellonio@gmail.com>
2024-12-10 16:13:37 +0100

    first draft of ex4

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - chekcs if the node is a leaf
+ * @node: node to check
+ * Return: 1 if node is a leaf, 0 if it is not
+ */
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node->left == NULL && node->right == NULL)
+		return (1);
+	else
+		return (0);
+}
