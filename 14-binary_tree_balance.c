de9e61533ae5026fcdcfffdbb444bc39e71fda0b
tramiNGY <ngytrami@gmail.com>
2024-12-11 19:17:12 +0100

    Added definition of binary_tree_height function inside folder

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -2,11 +2,37 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_height - measures the height of a tree
+ * @tree: pointer to the root
+ * Return: either i or j depending on the biggest one, -1 if fail
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t i;
+	size_t j;
+
+	i = 0;
+	j = 0;
+	if (tree == NULL)
+		return (0);
+	if (tree->left)
+		i += 1 + binary_tree_height(tree->left);
+	if (tree->right)
+		j += 1 + binary_tree_height(tree->right);
+
+	if (i > j)
+		return (i);
+	else
+		return (j);
+}
+
+/**
  * binary_tree_balance - measures the balance factor
  * of a binary tree
  * @tree: pointer to the root node of the tree
  * Return: return in balance factor or 0 if tree NULL
-*/
+ */
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
