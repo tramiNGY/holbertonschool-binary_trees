5b6084c0a26c83b782d4f56397cdd5e7fa227274
totomus83 <tom.dibellonio@gmail.com>
2024-12-12 15:11:36 +0100

    fourth draft

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -33,9 +33,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
+	size_t size;
+
+	size = 0;
 	if (tree == NULL)
 		return (0);
-	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+	size = 1 + (binary_tree_height(tree->left) + binary_tree_height(tree->right));
+	return (size);
 }
 
 /**
